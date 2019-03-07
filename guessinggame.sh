141daddc8d347fc4e01637f11b5e164c606f9208
jlewis18 <48249601+jlewis18@users.noreply.github.com>
2019-03-07 12:09:04 -0500

    Add files via upload

---
+++ guessinggame.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+function congrats {
+	echo "Congratulations!! You have given the right answer."
+
+}
+
+control=0
+
+while [[ $control -eq 0 ]]
+do
+	echo "Please enter the number of files in the current directory;"
+	read response
+	echo "You entered : $response"
+	numfiles=(*)
+	numfiles=${#numfiles[@]}
+	echo "the number of files is: $numfiles"
+	if [[ response -eq numfiles ]]
+	then
+	
+	congrats
+	control=1
+	elif [[ response -gt numfiles ]]
+	then
+		echo "too high a number."
+	else
+		echo "too low a number."
+	fi
+done 
---
+++ makefile
@@ -0,0 +1,7 @@
+README.md: guessinggame.sh
+	touch README.md
+	echo "# UNIX WorkBench peer-graded course assignment" > README.md
+	date >> README.md
+	echo "  " >> README.md
+	wc -1 < guessinggame.sh >> README.md
+
